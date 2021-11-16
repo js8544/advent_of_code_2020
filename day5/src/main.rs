d7f3b7acb0533514636dd582fe1e588ed7a3925b
jinshang <jinshang@tencent.com>
2021-11-16 19:14:18 +0800

    part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let ans = include_str!("input.txt")
+        .lines()
+        .map(compute_line)
+        .max()
+        .unwrap();
+    print!("ans: {}\n", ans);
+}
+
+fn compute_line(line: &str) -> u32 {
+    parse_str(&line[..7], (b'F', b'B')) * 8 + parse_str(&line[7..], (b'L', b'R'))
+}
+
+fn parse_str(str: &str, chars: (u8, u8)) -> u32 {
+    let mut num = 0;
+    str.as_bytes()
+        .iter()
+        .rev()
+        .enumerate()
+        .for_each(|(i, &char)| {
+            if char == chars.1 {
+                num |= 1 << i;
+            }
+        });
+    num
 }
