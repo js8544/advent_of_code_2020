b879322b239966c88f37ea610dc70deadef0f5dc
Jin Shang <shangjin1997@gmail.com>
2021-12-03 14:34:05 +0800

    day10part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,20 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
-    let mut arr: Vec<u64> = vec![0u64];
-    arr.extend(
-        include_str!("input.txt")
-            .lines()
-            .map(|line| line.parse::<u64>().unwrap()),
-    );
+    let mut arr: Vec<u64> = include_str!("input.txt")
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect();
     arr.sort_unstable();
     println!("sorted: {:?}", arr);
-    let (one_cnt, three_cnt) = arr.windows(2).fold((0, 1), |(one_cnt, three_cnt), window| {
-        let diff = window[1] - window[0];
-        (one_cnt + (diff == 1) as u64, three_cnt + (diff == 3) as u64)
+    let last = arr.last().unwrap();
+
+    let mut init: HashMap<u64, u64> = HashMap::new();
+    init.insert(0, 1);
+    let cnt_map = arr.iter().fold(init, |mut acc_map, &val| {
+        let cnt = (1u64..4.min(val + 1)).fold(0, |acc, diff| match acc_map.get(&(val - diff)) {
+            Some(v) => acc + v,
+            None => acc,
+        });
+        println!("val: {}, cnt: {}", val, cnt);
+        acc_map.insert(val, cnt);
+        acc_map
     });
-    println!(
-        "one: {}, three: {}, ans: {}",
-        one_cnt,
-        three_cnt,
-        one_cnt * three_cnt
-    );
+
+    println!("ans: {}", cnt_map.get(last).unwrap());
 }
